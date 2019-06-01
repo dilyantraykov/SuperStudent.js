43698cb86ac65b3b488874adb8d3f48dde9b6de0
Alexandar <a.dedelyanov@mail.bg>
2019-06-01 19:21:21 +0300

    test2 :D

---
+++ SuperStudent/src/js/main.js
@@ -0,0 +1,48 @@
+import Compositor from './Compositor.js';
+import Timer from './Timer.js';
+import {loadLevel} from './loaders.js';
+import {createMario} from './entities.js';
+
+import Keyboard from './KeyboardState.js';
+
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+Promise.all([
+    createMario(),
+    loadLevel('1.1'),
+])
+.then(([mario, level]) => {
+
+    const gravity = 2000;
+    mario.pos.set(64, 180);
+    mario.vel.set(200, -600);
+
+    level.entities.add(mario);
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+    });
+    input.listenTo(window);
+
+    const timer = new Timer(1/60);
+    timer.update = function update(deltaTime) {
+        level.update(deltaTime);
+
+        level.comp.draw(context);
+
+        mario.vel.y += gravity * deltaTime;
+    }
+
+    timer.start();
+});
+
+
+
