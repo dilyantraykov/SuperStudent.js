d460db319f375f016b8f10a655a619a82521a505
dilyantraykov <dtraykov94@gmail.com>
2019-06-10 02:24:26 +0300

    Background Patterns & Layers. Closes #5.

--- src/js/Level.js
+++ src/js/Level.js
@@ -1,6 +1,5 @@
 import Compositor from './Compositor.js';
 import TileCollider from './TileCollider.js';
-import {Matrix} from './math.js';
 
 export default class Level{
 	constructor(){
@@ -10,8 +9,11 @@ export default class Level{
 		
 		this.comp = new Compositor();
 		this.entities = new Set();
-		this.tiles = new Matrix();
-		this.tileCollider = new TileCollider(this.tiles);
+		this.tileCollider = null;
+	}
+
+	setCollisionGrid(matrix) {
+		this.tileCollider = new TileCollider(matrix);
 	}
 
 	update(deltaTime)
--- src/js/layers.js
+++ src/js/layers.js
@@ -1,6 +1,7 @@
-export function createBackgroundLayer(level, sprites) {
-	const tiles = level.tiles;
-	const resolver = level.tileCollider.tiles;
+import TileResolver from "./TileResolver.js";
+
+export function createBackgroundLayer(level, tiles, sprites) {
+	const resolver = new TileResolver()
 	
 	const buffer = document.createElement('canvas');
     buffer.width = 256 + 16;
@@ -8,10 +9,8 @@ export function createBackgroundLayer(level, sprites) {
 
     const context = buffer.getContext('2d');
 
-	let startIndex, endIndex;
-	function redraw(drawFrom, drawTo) {
-		startIndex = drawFrom;
-        endIndex = drawTo;
+	function redraw(startIndex, endIndex) {
+		context.clearRect(0, 0, buffer.width, buffer.height);
 
         for (let x = startIndex; x <= endIndex; ++x) {
             const col = tiles.grid[x];
@@ -26,10 +25,6 @@ export function createBackgroundLayer(level, sprites) {
 			}
 		}
 	}
-
-    level.tiles.forEach((tile, x, y) =>{
-        sprites.drawTile(tile.name, context, x, y);
-    });
 
     return function drawBackgroundLayer(context, camera) {
 		const drawWidth = resolver.toIndex(camera.size.x);
