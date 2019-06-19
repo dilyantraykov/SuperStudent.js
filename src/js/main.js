e0b45ee01d08ba5ae3f2d1257be80d2acf7c96ff
dilyantraykov <dtraykov94@gmail.com>
2019-06-19 18:53:48 +0300

    JSON Entity Spawning. Closes #6.

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,21 @@
 import Camera from './Camera.js';
 import Timer from './Timer.js';
-import { loadLevel } from './loaders/level.js';
+import { createLevelLoader } from './loaders/level.js';
 import { loadEntities } from './entities.js';
 import { setupKeyboard } from './input.js';
 import { createCollisionLayer } from './layers.js';
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
-
-Promise.all([
-    loadEntities(),
-    loadLevel('1.1'),
-])
-.then(([factory, level]) => {
+async function main(canvas) {
+    const context = canvas.getContext('2d');
+    const entityFactory = await loadEntities();
+    const loadLevel = await createLevelLoader(entityFactory);
+    const level = await loadLevel('1.1');
+    
 	const camera = new Camera();
     window.camera = camera;
 
-    const mario = factory.mario();
+    const mario = entityFactory.mario();
     mario.pos.set(64, 180);
-
-    const goomba = factory.goomba();
-    goomba.pos.x = 220;
-    level.entities.add(goomba);
-
-    const koopa = factory.koopa();
-    koopa.pos.x = 260;
-    level.entities.add(koopa);
 	
     level.entities.add(mario);
 
@@ -46,7 +36,7 @@ Promise.all([
     }
 
     timer.start();
-});
+};
 
-
-
+const canvas = document.getElementById('screen');
+main(canvas);
