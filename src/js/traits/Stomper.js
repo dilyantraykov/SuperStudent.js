4d84d22071a9ee3c6b78d5fda8e175af8eb64963
Tihomir <t.choki@abv.bg>
2019-06-23 19:25:09 +0300

    Added font, score count and timer! All videos done!

--- src/js/traits/Stomper.js
+++ src/js/traits/Stomper.js
@@ -1,27 +1,27 @@
-import { Trait, Sides } from '../Entity.js';
+import { Sides, Trait } from '../Entity.js';
 
 export default class Stomper extends Trait {
     constructor() {
         super('stomper');
-		this.bounceSpeed=400;
-    }
-    
-	bounce(us, them)
-	{		
-		us.bounds.bottom=them.bounds.top;
-		us.vel.y=-this.bounceSpeed;		
-	}	
-	
-	collides(us, them)
-	{
-		if (!them.killable || them.killable.dead) {
-			return;
-		}
+        this.bounceSpeed = 400;
 
-		if(us.vel.y>them.vel.y)
-		{
-			this.bounce(us,them);
-		}
-	}
-	
+        this.onStomp = function () {
+        }
+    }
+
+    bounce(us, them) {
+        us.bounds.bottom = them.bounds.top;
+        us.vel.y = -this.bounceSpeed;
+    }
+
+    collides(us, them) {
+        if (!them.killable || them.killable.dead) {
+            return;
+        }
+
+        if (us.vel.y > them.vel.y) {
+            this.bounce(us, them);
+            this.onStomp(us, them);
+        }
+    }
 }
